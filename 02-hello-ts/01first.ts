a1f8109b50cb96d69a6a8e586efac6c115d82b8c
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-23 21:04:16 +0800

    new init

--- 02-hello-ts/01first.ts
+++
@@ -1,2 +0,0 @@
-let heroName: string = 'zhangsan';
-alert(heroName);
--- 03-varible-types/01-primitives-types.ts
+++
@@ -1,6 +0,0 @@
-// simple types
-let user: string = "zhangsan"
-let age: number = 18
-let gender: boolean = true
-
-console.log("simple types::: ", gender)
